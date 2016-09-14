413ef2febf7b37d90bd48308b8c88f10b7a55c6b
324039924 <324039924@C-718T0R1.tdsb.on.ca>
2016-09-14 12:45:30 -0400

    AboutMe

---
+++ AboutMe/src/AboutMe.java
@@ -0,0 +1,12 @@
+
+public class AboutMe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Brendan Trinh");
+		System.out.println("Ms.Kemp");
+		System.out.println("Runnymeade C.I");
+		System.out.println("\"Go Ravens\"");
+	}
+
+}
