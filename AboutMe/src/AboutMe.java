965e040db456a1029aca1481c1a7668adc5a5003
324039924 <324039924@C-718T0R1.tdsb.on.ca>
2016-09-14 13:12:10 -0400

    AboutMe Part 2

--- AboutMe/src/AboutMe.java
+++ AboutMe/src/AboutMe.java
@@ -6,7 +6,13 @@ public class AboutMe {
 		System.out.println("Brendan Trinh");
 		System.out.println("Ms.Kemp");
 		System.out.println("Runnymeade C.I");
-		System.out.println("\"Go Ravens\"");
+		System.out.println("\"Go Ravens\"\n");
+		
+		System.out.format("%-10s %13s %17s","Weekdays","Class Name","Start/End\n");
+		System.out.format("%-10s %8s %13s","Monday-Friday","BAF3M/BAT4M1-01","8:45-10:02\n");
+		System.out.format("%-10s %8s %19s","Monday-Friday","SCH3U1-01","10:07-11-24\n");
+		System.out.format("%-10s %8s %16s","Monday-Friday","ICS3C/3U1-01","12:26-13:43\n");
+		System.out.format("%-10s %8s %18s","Monday-Friday","MCF3M1-01","13:48-15:05");
 	}
 
 }
