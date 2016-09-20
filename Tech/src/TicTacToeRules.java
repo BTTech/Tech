9ab1f25c5e0ad8a747e0747fe180847b9f4b36f3
324039924 <324039924@C-718T0R1.tdsb.on.ca>
2016-09-20 12:44:06 -0400

    TicTacToe Rules

---
+++ Tech/src/TicTacToeRules.java
@@ -0,0 +1,23 @@
+
+public class TicTacToeRules {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.format("TicTacToe Rules\n");
+		System.out.format("\n");
+		System.out.format("TicTacToe is a game for 2 players.\n");
+		System.out.format("\n");
+		System.out.format("1. A grid of 9 is drawn.\n");
+		System.out.format("\n");
+		System.out.format("2. The first player draws an X in a empty square.\n");
+		System.out.format("\n");
+		System.out.format("3. The second player draws an O in an empty square.\n");
+		System.out.format("\n");
+		System.out.format("4. Steps 2 and 3 are repeated untill a TicTac parttern is created:.\n");
+		System.out.format("\n");
+		System.out.format("Pattern can be diagonal, horzontal, or vertical.\n");
+		System.out.format("\n");
+		System.out.format("5. A winning pattern is completed by frawing a line through the pattern.\n");
+	}
+
+}
