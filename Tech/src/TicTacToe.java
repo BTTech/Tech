8e17b4402f48e6af63d14c1241f9240a96fc7e7e
324039924 <324039924@C-718T0R1.tdsb.on.ca>
2016-09-20 12:30:45 -0400

    TicTacToe

---
+++ Tech/src/TicTacToe.java
@@ -0,0 +1,19 @@
+
+public class TicTacToe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.format("%7s %6s","|","|\n");
+		System.out.format("%7s %6s","|","|\n");
+		System.out.format("%7s %6s","|","|\n");
+		System.out.format("-------------------\n");
+		System.out.format("%7s %6s","|","|\n");
+		System.out.format("%7s %2s %3s","|","X","|\n");
+		System.out.format("%7s %6s","|","|\n");
+		System.out.format("-------------------\n");
+		System.out.format("%7s %6s","|","|\n");
+		System.out.format("%7s %6s","|","|\n");
+		System.out.format("%7s %6s","|","|\n");
+	}
+
+}
