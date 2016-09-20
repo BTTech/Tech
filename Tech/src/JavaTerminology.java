1faa085de9dcaf3dedcb174e6b76aef14b0a1dae
324039924 <324039924@C-718T0R1.tdsb.on.ca>
2016-09-20 12:56:39 -0400

    Java Terminology

---
+++ Tech/src/JavaTerminology.java
@@ -0,0 +1,17 @@
+
+public class JavaTerminology {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.format("Class: The description of an object\n");
+		System.out.format("\n");
+		System.out.format("Comment: Text that provides information to the reader of the program code\n");
+		System.out.format("\n");
+		System.out.format("Execute: To run a program\n");
+		System.out.format("\n");
+		System.out.format("Bytecode: Compiled java source code\n");
+		System.out.format("\n");
+		System.out.format("Argument: Data passed to a method for processing\n");
+	}
+
+}
