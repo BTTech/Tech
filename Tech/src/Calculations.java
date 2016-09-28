abdcb7529bf0d7b2c2197ae181dadfae57a523da
324039924 <324039924@C-718T0R1.tdsb.on.ca>
2016-09-28 12:53:16 -0400

    Calculations

---
+++ Tech/src/Calculations.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Calculations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan=new Scanner(System.in);
+		int num1,num2,num3,num4,num5;
+		
+		System.out.println("Enter Number:");
+		
+		num1=scan.nextInt();
+		num2=scan.nextInt();
+		num3=scan.nextInt();
+		num4=scan.nextInt();
+		num5=scan.nextInt();
+		
+		System.out.println("The sum of all five numbers "+ (num1+num2+num3+num4+num5));
+		System.out.println("The result after subtracting the second number from the third is "+ (num2-num3));
+		System.out.println("The product of the first and fith is "+ (num1*num5));
+		System.out.println("The quotient of the forth number divided by the second number is "+ (num4/num2));
+		System.out.println("The remainder when dividing the forth number by the second number is "+ (num4%num2));
+	}
+
+}
