bb3aaf8206cef596a304a0430ecb6a4950aea274
324039924 <324039924@C-718T0R1.tdsb.on.ca>
2016-10-12 12:50:48 -0400

    Food

--- Tech/src/Food.java
+++ Tech/src/Food.java
@@ -16,28 +16,28 @@ public class Food {
 		double total;
 		double amountTendered;
 		
-		System.out.print("Enter the number of burgers:" );
+		System.out.print("Enter the number of burgers: " );
 		burger=scan.nextDouble() * 1.69;
 		
-		System.out.print("Enter the number of fries:" );
+		System.out.print("Enter the number of fries: " );
 		fries=scan.nextDouble() * 1.09;
 		
-		System.out.print("Enter The number of sodas:");
+		System.out.print("Enter The number of sodas: ");
 		soda=scan.nextDouble() * 0.99;
 		
-		System.out.println("Total before tax:" + (burger+fries+soda));
+		System.out.println("Total before tax: $" + (df.format(burger+fries+soda)));
 		beforeTax=burger+fries+soda;
 		
-		System.out.println("Tax:" + (beforeTax * 0.065));
+		System.out.println("Tax: $" +(df.format (beforeTax * 0.065)));
 		tax=beforeTax * 0.065;
 		
-		System.out.println("Total with tax:" + (beforeTax+tax));
+		System.out.println("Total with tax: $" +(df.format (beforeTax+tax)));
 		total=tax+beforeTax;
 		
 		System.out.print("Enter amount tendered:");
 		amountTendered=scan.nextDouble();
 		
-		System.out.println("Change" + (amountTendered - total));
+		System.out.println("Change: $" + (df.format(amountTendered - total)));
 		
 	}
 
