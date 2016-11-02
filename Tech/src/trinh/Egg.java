3cf737499ae3012f9baac38cd4f61fe5d62273c5
324039924 <324039924@C-718T0R1.tdsb.on.ca>
2016-11-02 12:45:06 -0400

    Fixed program

--- Tech/src/trinh/Egg.java
+++ Tech/src/trinh/Egg.java
@@ -14,32 +14,35 @@ public class Egg {
 		Scanner scan=new Scanner(System.in);
 		DecimalFormat df = new DecimalFormat ("0.00");
 
+		int dozen;
 		int egg;
-
+		
 		System.out.print("Enter Amount of Dozen Eggs: ");
+		dozen=scan.nextInt();
+		System.out.print("Enter Amount of Extra Eggs: ");
 		egg=scan.nextInt();
 
 		if (egg <=3){
 			System.out.println(" Price per dozen egg is: $0.50");
-			System.out.println("Total cost is: $" +df.format (egg * 0.50));
+			System.out.println("Total cost is: $" +df.format (dozen * 0.50 +( egg * 0.04166666666)));
 		}
 
 		else if ( egg <= 5){
 			System.out.println("Price per dozen egg is: $0.45");
-			System.out.println("Total cost is: $" +df.format (egg * 0.45));
+			System.out.println("Total cost is: $" +df.format (dozen * 0.45 +( egg * 0.0375)));
 		}
 
 		else if ( egg <=10){
 			System.out.println("Price per dozen egg is: $0.40");
-			System.out.println("Total cost is: $" +df.format (egg * 0.40));
+			System.out.println("Total cost is: $" +df.format (dozen * 0.40 +( egg * 0.03333333333)));
 		}
 
 		else if ( egg >=11){
 			System.out.println("Price per dozen egg is: $0.35");
-			System.out.println("Total cost is: $" +df.format (egg * 0.35));
+			System.out.println("Total cost is: $" +df.format (dozen * 0.35 +( egg * 0.02916666666)));
 		}
 
-		System.out.println("Total Amount of eggs: " + egg * 12);
+		System.out.println("Total Amount of eggs: " + (dozen * 12 +( egg)));
 
 
 	}
