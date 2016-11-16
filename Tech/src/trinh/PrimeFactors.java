6044888af01f8207e828a08d5dc609fe9a2cb3b2
324039924 <324039924@C-718T0R1.tdsb.on.ca>
2016-11-16 13:07:40 -0500

    PrimeFactors

---
+++ Tech/src/trinh/PrimeFactors.java
@@ -0,0 +1,34 @@
+package trinh;
+
+import java.util.Scanner;
+
+public class PrimeFactors {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scan=new Scanner(System.in);
+		
+		System.out.print("Enter Number");
+		
+		int userInput = scan.nextInt();
+		
+		int counter=2;
+		
+		while (counter <= userInput)
+		{
+			if (userInput % counter == 0)
+			{
+			System.out.println(counter);
+			userInput=(userInput/counter);
+			}
+		
+			else{
+		counter = counter + 1;
+			}
+		}
+		
+		
+	}
+
+}
