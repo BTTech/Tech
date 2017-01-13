d4e01632cbbc75d4a132dcac088ae539c54f70e6
324039924 <324039924@C-718T0R1.tdsb.on.ca>
2017-01-13 12:41:00 -0500

    HappyAndYouKnowIt

---
+++ Tech/src/trinh/HappyAndYouKnowIt.java
@@ -0,0 +1,93 @@
+package trinh;
+
+
+public class HappyAndYouKnowIt {
+
+	public static void main(String[] args) {
+		
+		String[] verses = new String [10];
+		
+		verses[0]= "clap your hands";
+		verses[1]= "stomp your feet";
+		verses[2]= "spin around";
+		verses[3]= "do the whip";
+		verses[4]= "take a dab";
+		verses[5]= "jump around";
+		verses[6]= "fall down";
+		verses[7]= "do the nae nae";
+		verses[8]= "slap your knee";
+		verses[9]= "run away";
+		
+		for (int i=0; i <10;i++){
+		
+		for (int x=0; x<2; x++){
+			System.out.println("If your happy and you know it " + verses[i]);
+		}
+		System.out.println("If your happy and you know it and your really want to show it");
+		System.out.println("If your happy and you know it "+ verses[i]+"\n");
+		}
+//		for (int x=1; x<2; x++){
+//		System.out.format("If your happy and you know it", verses[1],"\n");
+//		x=x+1;
+//		}
+//		System.out.format("If your happy and you know it and your really want to show it\n");
+//		System.out.format("If your happy and you know it", verses[1],"\n");
+//		
+//		for (int x=1; x<2; x++){
+//		System.out.format("If your happy and you know it", verses[2],"\n");
+//		x=x+1;
+//		}
+//		System.out.format("If your happy and you know it and your really want to show it\n");
+//		System.out.format("If your happy and you know it", verses[2],"\n");
+//		
+//		for (int x=1; x<2; x++){
+//		System.out.format("If your happy and you know it", verses[3],"\n");
+//		x=x+1;
+//		}
+//		System.out.format("If your happy and you know it and your really want to show it\n");
+//		System.out.format("If your happy and you know it", verses[3],"\n");
+//		
+//		for (int x=1; x<2; x++){
+//		System.out.format("If your happy and you know it", verses[4],"\n");
+//		x=x+1;
+//		}
+//		System.out.format("If your happy and you know it and your really want to show it\n");
+//		System.out.format("If your happy and you know it", verses[4],"\n");
+//		
+//		for (int x=1; x<2; x++){
+//		System.out.format("If your happy and you know it", verses[5],"\n");
+//		x=x+1;
+//		}
+//		System.out.format("If your happy and you know it and your really want to show it\n");
+//		System.out.format("If your happy and you know it", verses[5],"\n");
+//		
+//		for (int x=1; x<2; x++){
+//		System.out.format("If your happy and you know it", verses[6],"\n");
+//		x=x+1;
+//		}
+//		System.out.format("If your happy and you know it and your really want to show it\n");
+//		System.out.format("If your happy and you know it", verses[6],"\n");
+//		
+//		for (int x=1; x<2; x++){
+//		System.out.format("If your happy and you know it", verses[7],"\n");
+//		x=x+1;
+//		}
+//		System.out.format("If your happy and you know it and your really want to show it\n");
+//		System.out.format("If your happy and you know it", verses[7],"\n");
+//		
+//		for (int x=1; x<2; x++){
+//		System.out.format("If your happy and you know it", verses[8],"\n");
+//		x=x+1;
+//		}
+//		System.out.format("If your happy and you know it and your really want to show it\n");
+//		System.out.format("If your happy and you know it", verses[8],"\n");
+//		
+//		for (int x=1; x<2; x++){
+//		System.out.format("If your happy and you know it", verses[9],"\n");
+//		x=x+1;
+//		}
+//		System.out.format("If your happy and you know it and your really want to show it\n");
+//		System.out.format("If your happy and you know it", verses[9],"\n");
+	}
+
+}
